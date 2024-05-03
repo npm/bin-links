1350dac71bf39ff8529a8f3040c6d78f17fd7cbd
Luke Karrys <luke@lukekarrys.com>
2024-05-03 14:07:17 -0700

    fix(linting): no-unused-vars

--- test/check-bins.js
+++ test/check-bins.js
@@ -12,22 +12,24 @@ const checkBins = requireInject('../lib/check-bins.js', {
 const o = { global: true, force: false, top: true }
 
 t.test('gets normalized', t =>
-  checkBins({ pkg: { bin: 'lib/foo.js', name: 'foo' }, ...o }))
+  t.resolves(checkBins({ pkg: { bin: 'lib/foo.js', name: 'foo' }, ...o })))
 
 t.test('ok if all ok', t =>
-  checkBins({ pkg: { bin: { foo: 'lib/foo.js' } }, ...o }))
+  t.resolves(checkBins({ pkg: { bin: { foo: 'lib/foo.js' } }, ...o })))
 
 t.test('no bin is fine', t =>
-  checkBins({ pkg: {}, ...o }))
+  t.resolves(checkBins({ pkg: {}, ...o })))
 
 t.test('always ok if forced', t =>
-  checkBins({ pkg: { bin: { foo: 'lib/foo.js', fail: 'fail.js' } }, ...o, force: true }))
+  // eslint-disable-next-line max-len
+  t.resolves(checkBins({ pkg: { bin: { foo: 'lib/foo.js', fail: 'fail.js' } }, ...o, force: true })))
 
 t.test('always ok if not global', t =>
-  checkBins({ pkg: { bin: { foo: 'lib/foo.js', fail: 'fail.js' } }, ...o, global: false }))
+  // eslint-disable-next-line max-len
+  t.resolves(checkBins({ pkg: { bin: { foo: 'lib/foo.js', fail: 'fail.js' } }, ...o, global: false })))
 
 t.test('always ok if not top', t =>
-  checkBins({ pkg: { bin: { foo: 'lib/foo.js', fail: 'fail.js' } }, ...o, top: false }))
+  t.resolves(checkBins({ pkg: { bin: { foo: 'lib/foo.js', fail: 'fail.js' } }, ...o, top: false })))
 
 t.test('fail if any fail', t =>
   t.rejects(checkBins({ pkg: { bin: { foo: 'lib/foo.js', fail: 'fail.js' } }, ...o })))
